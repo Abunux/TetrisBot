ed4df168c1865463c9124250b956a931c8ce2ae7
Abunux <abunux@gmail.com>
2019-05-21 13:31:37 +0200

    Ajout de paramètres d'initialisation

--- source/tetris_RLenv.py
+++ source/tetris_RLenv.py
@@ -24,9 +24,10 @@ from time import *
 class TetrisEnv(TetrisEngine):
     """ Environnement à la OpenAI Gym pour implémenter le reinforcement learning """
 
-    def __init__(self, max_blocks=0, random_generator_seed=None, agent_name="", agent_description=""):
-        super().__init__(max_blocks=max_blocks,
-                         base_blocks_bag=CLASSIC_BLOCK_BAG,
+    def __init__(self, width=10, height=22, max_blocks=0, base_blocks_bag=CLASSIC_BLOCK_BAG,
+                 random_generator_seed=None, agent_name="", agent_description=""):
+        super().__init__(width=width, height=height, max_blocks=max_blocks,
+                         base_blocks_bag=base_blocks_bag,
                          temporisation=0, silent=True, random_generator_seed=random_generator_seed,
                          agent_name=agent_name, agent_description=agent_description)
         self.action_space = ['L', 'R', 'D', 'H', 'T', 'N']
@@ -35,7 +36,9 @@ class TetrisEnv(TetrisEngine):
 
     def reset(self):
         """ Réinitialise l'environnement """
-        self.__init__(max_blocks=self.max_blocks, random_generator_seed=self.random_generator_seed,
+        self.__init__(width=self.width, height=self.height, max_blocks=self.max_blocks,
+                      base_blocks_bag=self.base_blocks_bag,
+                      random_generator_seed=self.random_generator_seed,
                       agent_name=self.agent_name, agent_description=self.agent_description)
         self.getNewBlock()
         self.moveBlockInDirection('')
@@ -75,8 +78,8 @@ class TetrisEnv(TetrisEngine):
 
 
 if __name__ == "__main__":
-    input()
-    env = TetrisEnv()
+    input("Press enter to start")
+    env = TetrisEnv(width=5, height=6, base_blocks_bag=DOMINO_BLOCK_BAG)
     env.reset()
     for i in range(10000):
         sleep(0.01)
